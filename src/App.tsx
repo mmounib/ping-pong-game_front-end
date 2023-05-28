dba2d08ff7faf2134a8fd1f9b788a00d6b14dbef
Mouad Mounib <mmounib@student.1337.ma>
2023-05-28 11:58:35 +0100

    Beginning of the Project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect, useContext } from 'react'
+import Navbar from './components/Navbar/Navbar'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <div className=' underline text-blue-900 text-3xl'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App

b62779e9877cf24ce4a8e7396370b4e5d3624689
Mouad Mounib <mmounib@student.1337.ma>
2023-05-31 09:06:30 +0100

    Still Updating The Code

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import img from '../../assets/4312098.jpg'
 
-import {MdKeyboardDoubleArrowRight as rightArrowIcon} from 'react-icons/md'
+import { MdKeyboardDoubleArrowRight as rightArrowIcon} from 'react-icons/md'
+import { VscAccount as AccountIcon } from 'react-icons/vsc'
 
 const HomePage = () => {
   
   return (
-    <div className=" flex flex-col bg-back-bg rounded-[10px] w-[80%] mx-auto max-w-[1400px] h-[75vh] max-sm:w-[calc(100vw_-_1rem)] max-md:w-[calc(90vw_-_2rem)]">
+    <div className=" home flex flex-col bg-secondary-gradient drop-shadow-sm rounded-[10px] w-[80%] mx-auto max-w-[1400px] h-[75vh] max-sm:w-[calc(100vw_-_1rem)] max-md:w-[calc(90vw_-_2rem)]">
       
       <div className=" rounded-t-[10px] h-[200px] contrast-75 saturate-75 bg-cover bg-no-repeat bg-center w-full bg-image-bg object-cover">
         {/* <img src={img} alt="" className=" w-full object-cover h-[200px] rounded-t-[10px]" /> */}
@@ -16,14 +17,64 @@ const HomePage = () => {
         </div>
       </div>
 
-      {/* <div className="  text-white flex justify-center text-center w-[80%] mx-auto mt-[5rem]">
-        <div className='flex-auto bg-custom-blue mr-14'>
-          <h1 className="text-2xl">Live Games</h1>
+      <div className="  text-white flex text-center w-[85%] mx-auto mt-[5rem]">
+        <div className='rounded-[10px] flex-1 bg-custom-bg-icon mr-16 pb-[5rem] flex flex-col justify-between shadow-md'>
+          <h1 className="text-2xl p-2">Live Games</h1>
+          <div className=" mt-6 flex gap-4 flex-col justify-center">
+            <div className="flex mx-auto">
+              <AccountIcon size={50}/>
+              <div className=" mx-12 flex items-center gap-2">
+                <span className=''>LIVE</span>
+                <span className='w-[11px] h-[12px] rounded-[50%] bg-red-700'></span>
+              </div>
+              <AccountIcon size={50}/>
+            </div>
+            <div className="flex mx-auto">
+              <AccountIcon size={50}/>
+              <div className=" mx-12 flex items-center gap-2">
+                <span className=''>LIVE</span>
+                <span className='w-[11px] h-[12px] rounded-[50%] bg-red-700'></span>
+              </div>
+              <AccountIcon size={50}/>
+            </div>
+            <div className="flex mx-auto">
+              <AccountIcon size={50}/>
+              <div className=" mx-12 flex items-center gap-2">
+                <span className=''>LIVE</span>
+                <span className='w-[11px] h-[12px] rounded-[50%] bg-red-700'></span>
+              </div>
+              <AccountIcon size={50}/>
+            </div>
+          </div>
+          {/* <span className='pt-14'>See More</span> */}
         </div>
-        <div className='flex-auto bg-custom-blue'>
-          <h1 className="text-2xl">Latest Results</h1>
+        <div className='rounded-[10px] flex-1 bg-custom-bg-icon pb-[5rem] shadow-md'>
+          <h1 className="text-2xl p-2">Latest Results</h1>
+          <div className="mt-6 flex gap-4 flex-col justify-center">
+          <div className="flex mx-auto">
+              <AccountIcon size={50}/>
+              <div className=" mx-12 flex items-center gap-2">
+               <span className='text-2xl'>1 - 0</span>
+              </div>
+              <AccountIcon size={50}/>
+            </div>
+            <div className="flex mx-auto">
+              <AccountIcon size={50}/>
+              <div className=" mx-12 flex items-center gap-2">
+               <span className='text-2xl'>1 - 5</span>
+              </div>
+              <AccountIcon size={50}/>
+            </div>
+            <div className="flex mx-auto">
+              <AccountIcon size={50}/>
+              <div className=" mx-12 flex items-center gap-2">
+               <span className='text-2xl'>3 - 2</span>
+              </div>
+              <AccountIcon size={50}/>
+            </div>
+          </div>
         </div>
-      </div> */}
+      </div>
     </div>
   )
 }
